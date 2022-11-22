7c244eeb11191e9eec877341b4c1d85469d6b853
ermekkaragulov0@gmail.com <ermek1karagulov>
2022-11-22 10:36:47 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Function Expression - ананимная функция
+// const newFunc = function(params){
+//     тело фунции
+// }
+// newFunc()
+
+const showMassage = function (message) {
+  console.log(message);
+};
+showMassage("Hello");
